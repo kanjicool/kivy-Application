ff5fadce664aedc9539ff261b52042f558aa42a8
kanjicool <kanjicool2077@gmail.com>
2024-01-08 21:37:49 +0700

    change Button Behavior

--- app/app_paint.py
+++ app/app_paint.py
@@ -2,6 +2,14 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.utils import get_color_from_hex
 from kivy.config import Config
+from kivy.uix.behaviors import ToggleButtonBehavior
+from kivy.uix.togglebutton import ToggleButton
+
+class MyButton(ToggleButton):
+    def _do_press(self):
+        if self.state == 'normal':
+            ToggleButtonBehavior._do_press(self)
+        
 
 
 class PaintApp(App):
@@ -17,6 +25,6 @@ if __name__ == "__main__":
     from kivy.core.window import Window
     Config.set('graphics', 'width', '960')
     Config.set('graphics', 'height', '540')
-    Window.clearcolor = get_color_from_hex("#ffffff")
+    Window.clearcolor = get_color_from_hex('#ffffff')
 
     PaintApp().run()
