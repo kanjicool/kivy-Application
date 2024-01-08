98981876fff2866f39de64647e249d78244f76fd
kanjicool <kanjicool2077@gmail.com>
2024-01-08 21:14:14 +0700

    add Config.set

--- app/app_paint.py
+++ app/app_paint.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.utils import get_color_from_hex
+from kivy.config import Config
+
 
 class PaintApp(App):
     def build(self):
@@ -13,6 +15,8 @@ class CanvasWidget(Widget):
 
 if __name__ == "__main__":
     from kivy.core.window import Window
+    Config.set('graphics', 'width', '960')
+    Config.set('graphics', 'height', '540')
     Window.clearcolor = get_color_from_hex("#ffffff")
 
     PaintApp().run()
