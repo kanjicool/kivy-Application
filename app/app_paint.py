5cd55652bd3677f47e0098fd45167f4e30924fc7
kanjicool <kanjicool2077@gmail.com>
2024-01-10 17:34:26 +0700

    add def on_touch_move

--- app/app_paint.py
+++ app/app_paint.py
@@ -12,7 +12,6 @@ class MyButton(ToggleButton):
             ToggleButtonBehavior._do_press(self)
         
 
-
 class PaintApp(App):
     def build(self):
         self.canvas_widget = CanvasWidget()
@@ -20,17 +19,22 @@ class PaintApp(App):
         return self.canvas_widget
 
 class CanvasWidget(Widget):
+    line_width = 2 
     def set_color(self, new_color):
         self.last_color = new_color
         self.canvas.add(Color(*new_color))
 
     def on_touch_down(self, touch):
         if Widget.on_touch_down(self, touch):
-            return print("yett")
+            return print("clear!!!")
         
         with self.canvas:
-            Line(circle=(touch.x, touch.y, 25), width = 4)
-        
+            touch.ud['current_line'] = Line(points=(touch.x, touch.y), width=self.line_width)
+    
+    def on_touch_move(self, touch):
+        if 'current_line' in touch.ud:
+            touch.ud['current_line'].points += (touch.x, touch.y)
+    
     def clear_canvas(self):
         saved = self.children[:]
         self.clear_widgets()
